b1cc6c55548e6182ac522923f5b70618932b9ff6
Damodar Lohani <dlohani48@gmail.com>
2023-03-03 17:35:00 +0545

    update to get all applications

--- lib/app_state.dart
+++ lib/app_state.dart
@@ -2,4 +2,4 @@ import 'package:device_apps/device_apps.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final appsProvider = FutureProvider<List<Application>>(
-    (ref) => DeviceApps.getInstalledApplications(includeAppIcons: true));
+    (ref) => DeviceApps.getInstalledApplications(includeAppIcons: true, onlyAppsWithLaunchIntent: true, includeSystemApps: true));
